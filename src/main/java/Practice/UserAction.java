bdd158a09ff5542fc8cdfa957511a8de9896e830
Boris Dubov <dubovbe@gmail.com>
2020-09-21 01:08:08 +0300

    Registration and authorization ver. 0.3 Action menu added

---
+++ src/main/java/Practice/UserAction.java
@@ -0,0 +1,72 @@
+package Practice;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class UserAction {
+    private String userLogin;
+    private boolean isUserValidated;
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userLogin) {
+        this.userLogin = userLogin;
+    }
+
+    public boolean isUserValidated() {
+        return isUserValidated;
+    }
+
+    public void setUserValidated(boolean userValidated) {
+        isUserValidated = userValidated;
+    }
+
+    public UserAction(String userLogin, boolean isUserValidated) {
+        this.userLogin = userLogin;
+        this.isUserValidated = isUserValidated;
+        if(!isUserValidated) {
+            System.out.println("You need Log in / Sign up first");
+        }
+    }
+
+    public void actionMenu() throws SQLException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Action menu\n" +
+                "1 - Show list of bank accounts | ???");
+        switch (scanner.nextInt()) {
+            case 1:
+                showListOfBankAccounts();
+                break;
+            default:
+                System.out.println("Incorrect command");
+        }
+    }
+
+    public void showListOfBankAccounts() throws SQLException {
+
+        if (isUserValidated()) {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                    "postgres",
+                    "admin");
+
+            PreparedStatement listOfBankAccountsStatement = connection.prepareStatement("select login, email, balance, currency, date\n" +
+                    "from account\n" +
+                    "inner join bank_account ba on account.id = ba.user_id\n" +
+                    "where login = ?;");
+            listOfBankAccountsStatement.setString(1, getUserLogin());
+            ResultSet resultSet = listOfBankAccountsStatement.executeQuery();
+            while (resultSet.next()) {
+                System.out.println(
+                        resultSet.getString("login") + "\t" +
+                                resultSet.getString("email") + "\t" +
+                                resultSet.getInt("balance") + "\t" +
+                                resultSet.getString("currency") + "\t" +
+                                resultSet.getDate("date")
+                );
+            }
+        }
+    }
+}
---
+++ src/main/java/Practice/UserValidation.java
@@ -0,0 +1,138 @@
+package Practice;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class UserValidation {
+    private boolean isUserValidated = false;
+    private String userLogin;
+    private String userPassword;
+    private String userEmail;
+
+    public boolean isUserValidated() {
+        return isUserValidated;
+    }
+
+    public void setUserValidated(boolean userValidated) {
+        isUserValidated = userValidated;
+    }
+
+    public String getUserLogin() {
+        return userLogin;
+    }
+
+    public void setUserLogin(String userName) {
+        this.userLogin = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public void menu() throws SQLException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1 - Log in | 2 - Sign up");
+        switch (scanner.nextInt()) {
+            case 1:
+                authorizationAccount();
+                break;
+            case 2:
+                registrationNewAccount();
+                break;
+            default:
+                System.out.println("Incorrect command");
+        }
+    }
+
+    public void registrationNewAccount() throws SQLException {
+        boolean successfulRegistration = false;
+        Scanner scanner = new Scanner(System.in);
+
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                "postgres",
+                "admin");
+
+        while (!successfulRegistration) {
+            System.out.println("Enter new login: ");
+            setUserLogin(scanner.nextLine());
+            PreparedStatement loginCheckStatement = connection.prepareStatement("select count(id)\n" +
+                    "from account\n" +
+                    "where login = ?;");
+            loginCheckStatement.setString(1, getUserLogin());
+            loginCheckStatement.executeQuery();
+            ResultSet resultSet = loginCheckStatement.executeQuery();
+
+            while (resultSet.next()) {
+                if (resultSet.getInt("count") == 0) {
+                    successfulRegistration = true;
+                } else {
+                    System.out.println("A user with this login already exists, try again");
+                }
+            }
+            if (!successfulRegistration) continue;
+
+            System.out.println("Enter new password: ");
+            setUserPassword(DigestUtils.md5Hex(scanner.nextLine()));
+            System.out.println("Enter your email: ");
+            setUserEmail(scanner.nextLine());
+
+            PreparedStatement userRegistrationStatement = connection.prepareStatement("insert into account(login, password, email) " +
+                    "values (?, ?, ?);");
+            userRegistrationStatement.setString(1, getUserLogin());
+            userRegistrationStatement.setString(2, getUserPassword());
+            userRegistrationStatement.setString(3, getUserEmail());
+            userRegistrationStatement.execute();
+            System.out.println("Successful registration");
+        }
+
+        connection.close();
+    }
+
+    public void authorizationAccount() throws SQLException {
+        boolean successfulAuthorization = false;
+        Scanner scanner = new Scanner(System.in);
+
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                "postgres",
+                "admin");
+
+        while (!successfulAuthorization) {
+            System.out.println("Enter your login: ");
+            setUserLogin(scanner.nextLine());
+            System.out.println("Enter your password: ");
+            setUserPassword(DigestUtils.md5Hex(scanner.nextLine()));
+            PreparedStatement userAuthorizationStatement = connection.prepareStatement("select count(id)\n" +
+                    "from account\n" +
+                    "where login = ? and password = ?;");
+            userAuthorizationStatement.setString(1, getUserLogin());
+            userAuthorizationStatement.setString(2, getUserPassword());
+            userAuthorizationStatement.executeQuery();
+            ResultSet resultSet = userAuthorizationStatement.executeQuery();
+
+            while (resultSet.next()) {
+                if (resultSet.getInt("count") > 0) {
+                    System.out.println("Successful authorization");
+                    successfulAuthorization = true;
+                } else {
+                    System.out.println("Invalid login or password, try again");
+                }
+            }
+        }
+        setUserValidated(successfulAuthorization);
+        connection.close();
+    }
+}
